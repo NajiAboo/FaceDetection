8eaa42111115bd3d6744e0f714e4548368391987
t0393 <naji.aboo@triassicsolutions.com>
2021-10-11 08:44:32 +0530

    detect faces

--- face_detection.py
+++ face_detection.py
@@ -1,4 +1,16 @@
 import cv2
 
+#Loading Cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade  = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+def detect(gray, frame):
+    """
+     Detect the face from the image 
+     gray: gray image of the original image
+     frame: original image
+    """
+    # detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    
